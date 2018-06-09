3d2464ace57af988ba4bb9d27b8ace1cb2628eb7
= <>
2018-06-09 13:54:25 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import com.sun.deploy.util.SystemUtils;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static String nome = "Valteir";
+    public static Integer idade = 22;
+    public static Boolean sexoMasculino = true;
+    public static Double peso = 74.54;
+    public static Integer dia = 4;
+    public static String s = sexo(sexoMasculino);
+
+    public static void main(String[]args){
+        System.out.println("Sexo: "+ s);
+        System.out.println("Olá, meu nome é " + nome + ",tenho " + idade + " anos de idade");
+        System.out.println(CalculaIdade(idade));
+        System.out.println(DiaSemana(dia));
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int num = scanner.nextInt();
+        System.out.println(AlgRomanos(num));*/
+
+        Integer num = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero: "));
+        String Resultado = AlgRomanos(num);
+        if (Resultado == "Numero inválido")
+            JOptionPane.showMessageDialog(null, Resultado);
+        else
+            JOptionPane.showMessageDialog(null, "O numero " +
+                          + num+ " convertido para numero Romano é "+AlgRomanos(num));
+    }
+
+    //VERIFICA ALG ROMANOS
+    public static String AlgRomanos(int num){
+        switch (num){
+            case 1: return "I";
+            case 2: return "II";
+            case 3: return "III";
+            case 4: return "IV";
+            case 5: return "V";
+            case 6: return "VI";
+            case 7: return "VII";
+            case 8: return "VIII";
+            case 9: return "IX";
+            case 10: return "X";
+            default: return "Numero inválido";
+        }
+    }
+
+    //CALCULA O DIA DA SEMANA
+    public static String DiaSemana(Integer dia){
+        switch (dia){
+            case 1: return "Domingo";
+            case 2: return "Segunda-Feira";
+            case 3: return "Terça-Feira";
+            case 4: return "Quarta-Feira";
+            case 5: return "Quinta-Feira";
+            case 6: return "Sexta-Feira";
+            case 7: return "Sabado";
+            default: return "Não é um dia da semana";
+        }
+    }
+
+    // CALCULA A IDADE (NOVINHO/ TIOZINHO/ TIOZÃO)
+    public static String CalculaIdade (int idade){
+        if (idade <= 18)
+            return "Novinho";
+        else if (idade <= 30)
+            return "Tiozinho";
+        else
+            return "Tiozão";
+    }
+
+    //VALIDA O SEXO
+    public static String sexo(Boolean s){
+        if (s == true)
+            return "Masculino";
+        else
+            return "Feminino";
+    }
+}
